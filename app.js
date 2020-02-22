5fee1f7d4b281cf71330f1db02e288bf28cc1a28
Neelkant Jain <neelkant.jain@bridgesgi.com>
2020-02-22 18:04:51 +0530

    fixed the buttons css

--- app.js
+++ app.js
@@ -1,61 +1,86 @@
 var executeMe = function () {
+    let menuArrayIDs = ["home", "skills", "project", "experience", "academics", "blog"];
+    let menuArray = ["home", "skills-row", "project-row", "experience-row", "academics-row", "blog-row"];
+    let finishedMapping = getMappedHeights(menuArray);
     $(window).scroll(function () {
-        var firstRowRef = $("#home");
-        var topOfRow = firstRowRef.offset().top;
-        var heightOfFirstElem = firstRowRef.height();
-        var bottomOfElem = topOfRow + heightOfFirstElem;
-
         var viewportTop = $(window).scrollTop();
         var viewportBottom = viewportTop + $(window).height();
-
-        var projectClassRef = $("#project-button");
-        var homeClassRef = $("#home-button");
-        var navBar = $("#navigation-bar");
-        if (viewportBottom > bottomOfElem + 150) {
-            projectClassRef.addClass('active');
-            homeClassRef.removeClass('active');
-            navBar.addClass("navbar-dark");
-            navBar.addClass("bg-dark");
-            navBar.addClass("transition-duration", "5s");
-            navBar.removeClass("bg-light");
-            navBar.removeClass("navbar-light");
-
+        var rightElement = getRightDivision(finishedMapping, viewportBottom)
+        deActivateWrongButtons(menuArrayIDs[rightElement.index], menuArrayIDs);
+        if (menuArrayIDs[rightElement.index] !== "home") {
+            manageRestOfCSS(menuArrayIDs[rightElement.index])
         } else {
-            projectClassRef.removeClass('active');
-            homeClassRef.addClass('active');
-            navBar.removeClass("navbar-dark");
-            navBar.removeClass("bg-dark");
-            navBar.addClass("bg-light");
-            navBar.addClass("navbar-light");
+            manageHomeCSS();
         }
 
     })
 };
+var getBottomHeight = function (id) {
+    var firstRowRef = $("#" + id);
+    var topOfRow = firstRowRef.offset().top;
+    var heightOfFirstElem = firstRowRef.height();
+    var bottomOfElem = topOfRow + heightOfFirstElem;
+    return bottomOfElem;
+}
+
+var manageHomeCSS = function () {
+    var homeClassRef = $("#home-button");
+    var navBar = $("#navigation-bar");
+    homeClassRef.addClass('active');
+    navBar.removeClass("navbar-dark");
+    navBar.removeClass("bg-dark");
+    navBar.addClass("bg-light");
+    navBar.addClass("navbar-light");
+}
+
+var deActivateWrongButtons = function (exceptThis, menuArrayIDs) {
+    menuArrayIDs.forEach(function (value) {
+        if (value !== exceptThis) {
+            $("#" + value + "-button").removeClass("active");
+        }
+    })
+}
+
+var manageRestOfCSS = function (id) {
+    var projectClassRef = $("#" + id + "-button");
+    var navBar = $("#navigation-bar");
+    projectClassRef.addClass('active');
+    navBar.addClass("navbar-dark");
+    navBar.addClass("bg-dark");
+    navBar.addClass("transition-duration", "5s");
+    navBar.removeClass("bg-light");
+    navBar.removeClass("navbar-light");
+}
+
+var getRightDivision = function (ids, bottomindex) {
+    for (let i = 0; i < ids.length; i++) {
+        if (ids[i] > bottomindex - 200) { return { index: i, measure: ids[i] }; }
+    }
+    return { index: ids.length - 1, measure: bottomindex }
+}
+var getMappedHeights = function (ids) {
+    let heightArray = new Array();
+    ids.forEach(element => {
+        heightArray.push(getBottomHeight(element))
+    });
+    return heightArray;
+}
 $(document).on('click', 'a[href^="#"]', function (e) {
-    // target element id
     var id = $(this).attr('href');
     if (id === "#carouselExampleIndicators")
         return;
     var hightOfNav = $("#navigation-bar").height();
-    // target element
     var $id = $(id);
     if ($id.length === 0) {
         return;
     }
-
-    // prevent standard hash navigation (avoid blinking in IE)
     e.preventDefault();
-
-    // top position relative to the document
     var pos = $id.offset().top - hightOfNav - 20;
-
-    // animated top scrolling
     $('body, html').animate({ scrollTop: pos });
 });
 $(document).ready(function () {
-    $("#project-2").hide();
+    executeMe();
 });
 var goToMyBlog = function () {
     location.href = "https://techvoker.wordpress.com";
 }
-executeMe();
